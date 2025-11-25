a3a71eb039037d758849bb713b409d37d12b097d
marcos <marcos.vinicius.dev3@gmail.com>
2025-11-25 19:48:42 -0300

    refactor: SecurityFilter set tenant id

--- agenda-facil-pro/src/main/java/api/agendafacilpro/infraestructure/web/security/SecurityFilter.java
+++ agenda-facil-pro/src/main/java/api/agendafacilpro/infraestructure/web/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import api.agendafacilpro.core.domain.entities.User;
 import api.agendafacilpro.core.exceptions.UserNotFoundException;
 import api.agendafacilpro.core.gateway.JwtTokenGateway;
 import api.agendafacilpro.core.gateway.UserGateway;
+import api.agendafacilpro.core.multitenancy.TenantContext;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -35,37 +36,48 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String token = this.recoverTokenFromCookie(request);
+        try {
+            String token = this.recoverToken(request);
 
-        if (token != null && jwtTokenGateway.isTokenValid(token)) {
+            if (token != null && jwtTokenGateway.isTokenValid(token)) {
+                UUID userId = jwtTokenGateway.extractUserId(token);
 
-            UUID userId = jwtTokenGateway.extractUserId(token);
+                User user = userGateway.findById(userId)
+                        .orElseThrow(() -> new UserNotFoundException("User not found"));
 
-            User user = userGateway.findById(userId)
-                    .orElseThrow(() -> new UserNotFoundException("Usuário do token não encontrado na base de dados"));
+                if (Boolean.TRUE.equals(user.getEnabled())) {
+                    List<String> permissions = userGateway.findPermissionsUserId(user.getId());
+                    var authorities = permissions.stream()
+                            .map(SimpleGrantedAuthority::new)
+                            .toList();
 
-            if (Boolean.TRUE.equals(user.getEnabled())) {
-                List<String> permissions = userGateway.findPermissionsUserId(user.getId());
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
 
-                var authorities = permissions.stream()
-                        .map(SimpleGrantedAuthority::new)
-                        .toList();
-
-                var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                    TenantContext.setTenant(user.getOrganizationId());
+                }
             }
-        }
 
-        filterChain.doFilter(request, response);
+            filterChain.doFilter(request, response);
+
+        } finally {
+            TenantContext.clear();
+        }
     }
 
-    private String recoverTokenFromCookie(HttpServletRequest request) {
-        if (request.getCookies() == null) return null;
+    private String recoverToken(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            return Arrays.stream(request.getCookies())
+                    .filter(c -> "access_token".equals(c.getName()))
+                    .map(Cookie::getValue)
+                    .findFirst()
+                    .orElse(null);
+        }
 
-        return Arrays.stream(request.getCookies())
-                .filter(cookie -> "access_token".equals(cookie.getName()))
-                .map(Cookie::getValue)
-                .findFirst()
-                .orElse(null);
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
     }
 }
